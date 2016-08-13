5601d0c3df7af6c01686891b2c5e8fa8846d15a4
사명구 <samyeong-gu@samyeongguui-MacBook.local>
2016-08-13 20:19:36 +0900

    강좌 6까지 설정, 로그 파일을 workspace 디렉터리에 만들도록 설정함.

--- src/main/java/net/slipp/web/HomeController.java
+++ src/main/java/net/slipp/web/HomeController.java
@@ -1,16 +1,21 @@
 package net.slipp.web;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class HomeController {
+	
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
 	// 
 	@RequestMapping("/")
 	public String home() {
+		log.debug("logback setting success!");
 		// 이동할 뷰의 이름 - home
 		// /home.jsp - suffix, prefix가 자동으로 붙는다.
 		// 결국 홈에 있는 home.jsp를 호출하게 된다.
